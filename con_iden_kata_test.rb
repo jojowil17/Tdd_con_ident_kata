507e620e3a1fbfdccacf76ecdb80d1d698ce8c0e
jojowil17 <jojowil32@gmail.com>
2015-09-09 19:50:11 -0400

    added two to the array in the test

--- con_iden_kata_test.rb
+++ con_iden_kata_test.rb
@@ -1,7 +1,6 @@
 require_relative "con_iden_kata.rb"
 require "minitest/autorun"
-class Consecutive_test <
-Minitest::Test
+class Consecutive_test < Minitest::Test
 
 
 def test_empty_arrays_return_zero
@@ -12,7 +11,12 @@ def test_empty_arrays_return_zero
 def test_array_with_one_element
      results = find_max_consecutive_identical_elements([1])
 	 assert_equal(1, results)
-end	 
+end
+	 
+def test_array_with_two_non_identical_elements
+    results = find_max_consecutive_identical_elements([1,2])
+	assert_equal(1, results)
+end
 	 
 	 
 	 
