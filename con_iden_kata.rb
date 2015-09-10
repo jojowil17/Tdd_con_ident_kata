7f42a8b261e9fd6cfabf43703d51afccf48053b4
jojowil17 <jojowil32@gmail.com>
2015-09-09 20:03:57 -0400

    we added two identical elements to the test

--- con_iden_kata.rb
+++ con_iden_kata.rb
@@ -2,7 +2,9 @@ def
    find_max_consecutive_identical_elements(any_array)
      if any_array.length == 0
 	         0
-	 else 
+	 elsif any_array[0] == any_array[1]
+	 2
+	 else
 	         1
     end
 end   
